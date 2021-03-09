2ba61f8e3447bc431b20c6fecf173df01a996f1e
Elisa Tumwebaze <tumwebazeelisa@gmail.com>
2021-03-09 10:25:41 +0300

    added other developed pages

---
+++ main.js
@@ -0,0 +1,35 @@
+let toggleNavStatus=false;
+let toggleNav=function(){
+	let getSidebar=document.querySelector(".nav-sidebar");
+	let getSidebarUl=document.querySelector(".nav-sidebar ul");
+	let getSidebarTitle=document.querySelector(".nav-sidebar span");
+	let getSidebarLinks=document.querySelectorAll(".nav-sidebar a");
+	let getMainContent=document.querySelector(".main-content")
+
+	if (toggleNavStatus===false) {
+		getSidebarUl.style.visibility="visible";
+		getSidebar.style.width="272px";
+		getMainContent.style.marginLeft="275px";
+		getSidebarTitle.style.opacity="0.5";
+
+		let arrayLength=getSidebarLinks.length;
+		for (let i = 0; i < arrayLength; i++) {
+			getSidebarLinks[i].style.opacity = "1";
+		}
+		toggleNavStatus = true;
+	}
+
+	else if (toggleNavStatus===true) {
+		
+		getSidebar.style.width="50px";
+		getMainContent.style.marginLeft="55px";
+		getSidebarTitle.style.opacity="0";
+
+		let arrayLength=getSidebarLinks.length;
+		for (let i = 0; i < arrayLength; i++) {
+			getSidebarLinks[i].style.opacity = "0";
+		}
+		toggleNavStatus = false;
+		getSidebarUl.style.visibility="hidden";
+	}
+}
